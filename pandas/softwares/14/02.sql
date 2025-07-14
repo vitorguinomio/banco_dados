cf3596fe801a5df2286b267b689f7a62b9fec079
vitorguinomio <vitorguinomio@gmail.com>
2025-07-14 18:59:22 +0000

    Foi feito o estudo ate a aula 7 de pl pgsql

--- pandas/softwares/14/02.sql
+++ pandas/softwares/14/02.sql
@@ -1,4 +1,4 @@
--- Active: 1751900289641@@127.0.0.1@5432@pl_pg
+-- Active: 1752513116454@@127.0.0.1@5432@psql
 -- Estrutura de uma Function no PLGSQL
 
 
@@ -68,25 +68,118 @@ INOUT para entrada e saida*/
 INSERT INTO professor (
     nome, dt_nasc, salario
 )VALUES
-('Raul Seixas', '02/02/1978', 12150.34),
-('Gil Gomes', '01/04/1962', 8642.60),
-('Renata Costa', '02/05/1988', 1250.30),
-('Renato Gil', '01/01/1997', 998.00),
-('Pedro Silva', '03/05/2001', 3500),
-('Raquel Souza', '29/10/1996', 2400),
-('Borges Bento', '11/11/1990', 6700.34),
-('Harry Potter', '01/04/1998', 998),
-('Adélia Moura', '01/12/1986', 12150.34),
-('Jimmy Page', '03/05/2001', 1200),
-('Marco Aurélio', '03/12/2000', 998),
-('Helena Silva', '02/01/1997', 998),
-('Fábio Duarte', '29/10/1995', 3200),
-('Mata Rocha', '03/12/2001', 3570),
-('Maria Carla', '02/01/1997', 1998),
-('Renato Feliz', '01/07/2001', 6789.34),
-('Lucas Sávio', '29/10/2000', 3410);
+('Raul Seixas', '02-02-1978', 12150.34),
+('Gil Gomes', '01-04-1962', 8642.60),
+('Renata Costa', '02-05-1988', 1250.30),
+('Renato Gil', '01-01-1997', 998.00),
+('Pedro Silva', '03-05-2001', 3500),
+('Raquel Souza', '10-10-1996', 2400),
+('Borges Bento', '11-11-1990', 6700.34),
+('Harry Potter', '01-04-1998', 998),
+('Adélia Moura', '01-12-1986', 12150.34),
+('Jimmy Page', '03-05-2001', 1200),
+('Marco Aurélio', '03-12-2000', 998),
+('Helena Silva', '02-01-1997', 998),
+('Fábio Duarte', '12-10-1995', 3200),
+('Mata Rocha', '03-12-2001', 3570),
+('Maria Carla', '02-01-1997', 1998),
+('Renato Feliz', '01-07-2001', 6789.34),
+('Lucas Sávio', '2-10-2000', 3410);
 
 
 -- functionpara mostra maior e menor e a media de salario
 
-CREATE OR REPLACE
+CREATE OR REPLACE FUNCTION min_avg_max (OUT menor numeric, OUT media numeric, OUT maior numeric)
+AS 
+$$
+BEGIN
+    SELECT min(salario) FROM professor into menor;
+    SELECT round(avg(salario),2)  FROM professor into media;
+    SELECT max (salario) FROM professor into maior;
+END;
+$$
+LANGUAGE PLPGSQL
+
+SELECT * FROM min_avg_max();
+
+
+
+-- Os comandos condicionais ( IF , THEN , ELSE , ELSIF)
+--END IF; FINALIZA O COMANDO
+
+CREATE OR REPLACE FUNCTION maisQueMedia (IN cod numeric) RETURNS BOOLEAN
+AS
+$$
+DECLARE
+    media real;
+    salario_prof real;
+BEGIN
+    SELECT round(avg(salario),2)  FROM professor into media;
+    SELECT salario FROM professor where id = cod into salario_prof;
+
+    if (salario_prof > media) then -->entao forma de continua descricao da frase
+        RETURN TRUE;
+    ELSE
+        return FALSE;
+    end if;
+END;
+$$ LANGUAGE PLPGSQL;
+
+SELECT maisQueMedia(1);
+
+SELECT * FROM professor;
+
+
+
+/*WHILE condiçao LOOP 
+    comando 1;
+    comando 2;
+    comando 3;
+    ...
+    END LOOP;*/
+
+
+CREATE OR REPLACE FUNCTION fatorial(n integer) RETURNS integer AS
+$$
+DECLARE
+    fat integer := 1;
+    i integer := 2;
+BEGIN
+    WHILE i <= n LOOP
+        fat := fat * i;
+        i := i + 1;
+    END LOOP;
+    RETURN fat;
+END;
+$$ LANGUAGE plpgsql;
+
+select fatorial(12);
+
+
+/*loop for
+    FOR varivael IN i..j (caso eu queira te um pulo especifico colcocar "BY K= quantidade de pulo")LOOP
+    comando 1;--
+                |--> comando são excutados exatamente quantidade de vezes que (j-i+1) até chegar a 0 ou 1
+    comando 2;--
+    ...
+    endo loop;
+    */
+
+CREATE OR replace FUNCTION  fatorial2(n integer) RETURNS INTEGER AS
+$$
+DECLARE
+    i INTEGER;
+    fat INTEGER := 1 ;
+BEGIN
+    if n < 2 THEN
+        RETURN fat;
+    end if;
+    for i in 2..n LOOP
+        fat := fat*i;
+    end loop;
+    RETURN fat;
+END;
+$$ LANGUAGE PLPGSQL
+
+
+select fatorial2(7);
